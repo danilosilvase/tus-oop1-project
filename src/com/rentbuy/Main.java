47b6fcff72885c1834942a38472bd6a0ca7d29a0
Danilo Silva <danilosilvase@gmail.com>
2024-11-02 08:32:30 +0000

    chore: update main class to validate property, house and aaprtment

--- src/com/rentbuy/Main.java
+++ src/com/rentbuy/Main.java
@@ -1,7 +1,14 @@
 package com.rentbuy;
 
+import com.rentbuy.property.House;
+import com.rentbuy.property.Apartment;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Rentbuy");
+        House house = new House("123 Green St, Dublin", 350000, true);
+        Apartment apartment = new Apartment("456 Blue Ave, Dublin", 250000, 5);
+
+        System.out.println("House Details: " + house.getDetails());
+        System.out.println("Apartment Details: " + apartment.getDetails());
     }
 }
